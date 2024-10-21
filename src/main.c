a759cce2593f76641a0bac359acf8a34670b2338
shishir-dey <shishir-dey@users.noreply.github.com>
2024-10-21 17:07:45 +0000

    deploy: cd150a4b9986fde1c7089a2afed0ab24c7583788

--- src/main.c
+++ src/main.c
@@ -1,11 +1,26 @@
 #include "linked_list.h"
 
-int main(void)
+node_t node1, node2, node3, node4, node5, node6;
+char* str1 = "Node 1";
+char* str2 = "Node 2";
+char* str3 = "Node 3";
+char* str4 = "Node 4";
+
+int main()
 {
-	Node *list = InitList();
-	AddNode(list, "Hello");
-	AddNode(list, "World");
-	DeleteList(list);
-	for (;;)
-		;
+    ll_init(&node1);
+    node1.data = str1;
+
+    node2.data = str2;
+    node3.data = str3;
+    ll_insert_at_head(&node2);
+    ll_insert_at_head(&node3);
+
+    node4.data = str4;
+    ll_insert_at_tail(&node4);
+
+    ll_delete_at_head();
+    ll_delete_at_tail();
+
+    return 0;
 }
